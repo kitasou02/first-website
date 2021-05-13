46ef8369e0313b329b342665c45ef2b934aecc23
kitasou02 <kitasou0226@gmail.com>
2021-05-13 17:12:29 +0900

    'jsの追加'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener('keydown', (event) => {
-    console.log(event.key);
-  }); 
+  switch (event.key) {
+    case 'ArrowUp': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
+
+      button.style.top = `${Number(button.style.top.split('px')[0]) - 10}px`;
+
+      break;
+    }
+45
+    case 'ArrowDown': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
+
+      button.style.top = `${Number(button.style.top.split('px')[0]) + 10}px`;
+
+      break;
+    }
+
+    case 'ArrowRight': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
+
+      button.style.left = `${Number(button.style.left.split('px')[0]) + 10}px`;
+
+      break;
+    }
+
+    case 'ArrowLeft': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
+
+      button.style.left = `${Number(button.style.left.split('px')[0]) - 10}px`;
+
+      break;
+    }
+  }
+});
